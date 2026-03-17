d36ac72170c761861a92e160b71769a248df4e71
lwazi Mokone <moknelwazi4@gmail.com>
2026-03-17 20:37:10 +0200

    Entered a if statement  learning control flow

--- node.js
+++ node.js
@@ -1,2 +1,8 @@
 const MainCharcter = "Lucas";
+console.log(MainCharcter);
 
+let gold = Number(prompt("How much gold do you have?"));
+
+if (gold > 100) {
+  console.log("You buy the Enchanted Blade!");
+}
