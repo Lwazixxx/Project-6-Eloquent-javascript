da5a9ace62f08a737fb02c507505a7164ff5ee0f
lwazi Mokone <moknelwazi4@gmail.com>
2026-03-17 21:24:17 +0200

    IF STATEMENTS CONTROL FLOW

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
-prompt("Enter Name");
 
-const MainCharcter = "Lucas";
+const gold = 150;
 
-console.log(MainCharcter);
+console.log("You have " + gold + " gold pieces.");
+
+prompt("What is your name?");
+
+confirm("Are you sure you want to do this?");
+
+console.log(confirm);
+
+console.log(prompt);
+
+let goldCOIN = Number(prompt("How much gold do you have?"));
+
+if (goldCOIN > 100) {
+  console.log("You buy the Enchanted Blade!");
+}
+
+if (goldCOIN < 100) {
+  console.log("You don't have enough gold to buy the Enchanted Blade.");
+}
+
+else {
+  console.log("You have exactly 100 gold pieces, you can buy the Enchanted Blade!");
+}
+
--- node.js
+++ node.js
@@ -1,8 +1,1 @@
-const MainCharcter = "Lucas";
-console.log(MainCharcter);
 
-let gold = Number(prompt("How much gold do you have?"));
-
-if (gold > 100) {
-  console.log("You buy the Enchanted Blade!");
-}
